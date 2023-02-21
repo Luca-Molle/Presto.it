5bd8764842384faff86d219d173e0f7dfdb58e19
Francesco <difinofrancesco@gmail.com>
2023-02-21 17:20:17 +0100

    creazione navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,9 @@
 <x-layout>
-    ciao a tutti
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1>Presto.it</h1>
+            </div>
+        </div>
+    </div>
 </x-layout>
