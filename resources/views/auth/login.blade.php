7f679355894d86f7d4675cc6d441374adf3c150b
Andrea <andreanicoletti831@gmail.com>
2023-02-21 18:50:40 +0100

    rettifica login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,15 +7,15 @@
                 <form action="{{route('login')}}" method="POST">
                     @csrf
                     <div class="mb-3">
-                        <label for="exampleInput" class="form-label">Email address</label>
-                        <input name="email" type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp" >
+                        <label for="email" class="form-label">Email address</label>
+                        <input name="email" type="email" class="form-control" id="Email" aria-describedby="emailHelp" >
                         <div id="emailHelp" class="form-text">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam maiores aperiam vel, eligendi voluptate totam!
                         </div>
                     </div>
                     <div class="mb-3">
-                    <label for="passwordInputPassword1" class="form-label">Password</label>
-                    <input name="password_confirmation" type="password" class="form-control" id="password_confirmation">
+                    <label for="password" class="form-label">Password</label>
+                    <input name="password" type="password" class="form-control" id="password">
                 </div>
                 <div class="mb-3 form-check">
                     <input name="remember" type="checkbox" class="form-check" id="exampleCheck1">
