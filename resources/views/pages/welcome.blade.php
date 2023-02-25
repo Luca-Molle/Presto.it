569bb0e68e24c21f1309241d78d713b231e6478a
Luca Molle <luca.molle87@gmail.com>
2023-02-25 17:54:26 +0100

    sistemazione homepage

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -42,7 +42,24 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-12">
+            
+            {{-- Prova colonne --}}
+            @foreach ($announcements as $announcement)
+                <div class="col-3 mt-4">
+                    <div class="column rounded bg-white mb-4 shadow p-3">
+                        <p class="textmain fw-bold text-center">{{ $announcement->category->name}}</p>
+                        <img src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/xps-notebooks/xps-15-9520/media-gallery/black/laptop-xps-9520-t-black-gallery-4.psd?fmt=pjpg&pscan=auto&scl=1&wid=3491&hei=2077&qlt=100,1&resMode=sharp2&size=3491,2077&chrss=full&imwidth=5000" class="img-fluid card-image" alt="foto">
+                        <div>
+                            <h4 class="fw-bold">{{ $announcement->title }}</h6>
+                            <h5 class="">€ {{ $announcement->price }}</h5>
+                            <button class="btn btn-outline-secondary shadow">Visualizza</button>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+
+            {{-- Versione elenco  --}}
+            {{-- <div class="col-12">
                 @foreach ($announcements as $announcement)
                 <div class="card mx-auto m-3 card-custom d-flex col-6 shadow ">
                     <div class="row ">
@@ -66,7 +83,7 @@
 
                 </div>
                 @endforeach
-            </div>
+            </div> --}}
         </div>
 
     </div>
