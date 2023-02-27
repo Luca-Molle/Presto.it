e6cadbd4daef4af712a6f28ce900b4779ddfdc4b
Luca Molle <luca.molle87@gmail.com>
2023-02-27 21:51:52 +0100

    import class RevisorController in routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnnouncementController;
 use App\Http\Controllers\FrontController;
+use App\Http\Controllers\RevisorController;
 use App\Http\Controllers\userPageController;
 
 /*
