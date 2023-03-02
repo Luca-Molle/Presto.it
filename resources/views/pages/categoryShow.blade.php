249c0b48b125f559e8f4570a84c485e3c478a10a
Giuseppe Mottola <giuseppemottola123@gmail.com>
2023-03-02 20:31:01 +0100

    aggiusti front-end sezione annunci GM

--- resources/views/pages/categoryShow.blade.php
+++ resources/views/pages/categoryShow.blade.php
@@ -12,24 +12,24 @@
 
 
             @forelse ($category->announcements as $announcement)
-                    <div class="card mx-auto m-3 card-custom d-flex col-6 ">
+                    <div class="bg-white my-2 shadow p-3 mt-5 ">
                         <div class="row ">
-                            <div class="col-6 d-flex align-items-center ">
-                                <img src="https://static.fanpage.it/wp-content/uploads/sites/6/2020/04/migliori-pc-da-gaming.jpg"
-                                    class="card-img-top rounded img-fluid" alt="...">
+                            <div class="col-6 mt-4 ">
+                                <img src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/xps-notebooks/xps-15-9520/media-gallery/black/laptop-xps-9520-t-black-gallery-4.psd?fmt=pjpg&pscan=auto&scl=1&wid=3491&hei=2077&qlt=100,1&resMode=sharp2&size=3491,2077&chrss=full&imwidth=5000"
+                                    class="img-fluid card-image p-2" alt="...">
                             </div>
-                            <div class="col-6">
+                            <div class="col-4 ms-4">
                                 <div class="card-body ">
-                                    <h4 class="card-title">{{ $announcement->title }}</h4>
-                                    <p class="card-text">{{ $announcement->price }} € </p>
-                                    <a href="{{ route('announcements.show', $announcement) }}"
-                                        class="btn btn-primary shadow">Visualizza</a>
-                                    <a href=""
-                                        class="border-dark pt-2 my-2 card-link shadow btn btn-success border-top">Categoria:
-                                        {{ $announcement->category->name }}</a>
-                                    <p class="card-footer">Pubblicato il:
+                                    <h4 class="card-title fw-bold">{{ $announcement->title }}</h4>
+                                    <p class="card-text textmain fw-bold">{{ $announcement->price }} € </p>
+                                    <p class="mt-5 card-footer">Pubblicato il:
                                         {{ $announcement->created_at->format('d/m/Y') }}
                                     </p>
+                                    <a href="{{ route('announcements.show', $announcement) }}"
+                                        class="btn btn-outline-secondary shadow mt-5">Visualizza</a>
+
+                                   
+                                    
                                 </div>
                             </div>
                         </div>
@@ -38,7 +38,7 @@
                         <div class="col-12 mt-2">
                         <p class="h1">Non sono presenti annunci per la categoria {{ $category->name }}</p>
                         <p class="h2">Pubblicane uno: <a href="{{ route('announcement.create') }}"
-                            class="btn btn-primary shadow">Nuovo Annuncio</a></p>
+                            class="btn maincolor text-white shadow">Nuovo Annuncio</a></p>
                         </div>
                     
                     
