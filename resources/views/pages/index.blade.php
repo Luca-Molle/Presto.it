4e310d5ba2d28a85892cf320d544bb8b92427c5a
Francesco <difinofrancesco@gmail.com>
2023-03-09 21:04:38 +0100

    f.e. card

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,15 +3,15 @@
     <div class="container">
         <div class="row  align-items-between">
             <div class=" col-4 ">
-                <h2 class="mt-5">Sezione filtri</h2>
+                <h2 class="mt-5 ">Sezione filtri</h2>
             </div>
             <div class=" col-8 ">
                 @forelse ($announcements as $announcement)
-                    <div class="bg-white my-2 shadow p-3 mt-5" data-aos="zoom-in">
-                        <div class="row">
+                    <div class="bg-white my-2 shadow p-3 mt-5 card-announcement" data-aos="zoom-in">
+                        <div class="row ">
                             <div class="col-6 mt-4">
-                                <img src="{{ !$announcement->images()->get()->isEmpty()? $announcement->images()->first()->getUrl(700,500) : 'https://blumagnolia.ch/wp-content/uploads/2021/05/placeholder-126.png' }}"
-                                class="img-fluid card-image rounded shadow mt-3" alt="foto"></a>
+                                <img src="{{ !$announcement->images()->get()->isEmpty()? $announcement->images()->first()->getUrl(700, 500): 'https://blumagnolia.ch/wp-content/uploads/2021/05/placeholder-126.png' }}"
+                                    class="img-fluid card-image rounded shadow mt-3" alt="foto"></a>
                             </div>
                             <div class="col-4 ms-4">
                                 <h4 class="fw-bold">{{ $announcement->title }}</h6>
@@ -24,7 +24,8 @@
                             </div>
                         </div>
                         <div class="d-flex align-items-center" style="height: 75px">
-                            <a class=" h-50 col-12 btn btn-outline-presto shadow" href="{{ route('categoryShow', $announcement->category) }}">
+                            <a class=" h-50 col-12 btn btn-outline-presto shadow"
+                                href="{{ route('categoryShow', $announcement->category) }}">
                                 <p class="textmain fw-bold text-center">{{ $announcement->category->name }}</p>
                             </a>
                         </div>
