16dce40114cea2d535757d93de868513222f3114
Luca Molle <luca.molle87@gmail.com>
2023-03-14 19:13:15 +0100

    rimozione vecchia tabella

--- resources/views/livewire/modal-delete.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="card-body bg-dark mt-5">
-    <p class="p-3 text-white fw-bold">Confermi l'eliminazione?</p>
-    <button class="btn btn-sm btn-outline-presto">Annulla</button>
-    <button class="btn btn-sm btn-danger" wire:click="destroy({{ $announcement->id }})" >Elimina</button>
-</div>
-
